dada6f8633d9d0b98f0b8d9b4db78f908719b774
Artur Dantas <arturdantas121@gmail.com>
2024-07-31 23:30:49 -0300

    atualizacao 2

---
+++ src/components/SobreCurso.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './SobreCurso.css'
+import internetglobal from '../assets/internetglobal.png'
+
+export default function(){
+    return(
+        <>
+        <div className="container-sobrec">
+            <img className="img-global" src={internetglobal} alt="imagem que representa globalização da telecomunicação"/>
+            <section>
+                <h1> Sobre o curso de Tecnologia em Telemática</h1>
+                <p className="texto-sobre-curso">A Telemática é uma área de estudo que integra telecomunicações e informática, focando na transmissão, processamento e armazenamento de informações. Esta disciplina abrange diversas tecnologias, como redes de computadores, internet, comunicação sem fio e sistemas embarcados. Com a crescente demanda por conectividade e a rápida evolução dos dispositivos inteligentes, a telemática se torna crucial para o desenvolvimento de soluções inovadoras em diversas indústrias, desde a automotiva até a saúde. Profissionais dessa área são responsáveis por projetar, implementar e gerenciar infraestruturas de comunicação que suportam o mundo digital, tornando-se peças-chave na transformação digital e na construção de uma sociedade mais conectada.</p>
+            </section>
+        </div>
+        </>
+    )
+}
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar"
 import SessaoApresentacao from '../components/SessaoApresentacao';
 import SessaoHabilid from '../components/SessaoHabilid';
 import Footer from '../components/Footer';
+import SobreCurso from '../components/SobreCurso';
 
 function Home() {
   return (
@@ -10,6 +11,7 @@ function Home() {
       <Navbar/>
       <SessaoApresentacao/>
       <SessaoHabilid/>
+      <SobreCurso/>
       <Footer/>
     </>
   );
